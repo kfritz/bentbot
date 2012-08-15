634190bf076bcf6f59bfe3894f4ebcadd11fdaa7
Dwayne Bent <me@dwayne.bent.io>
2012-08-14 21:17:18 -0400

    Add support for "I'm Feeling Lucky" queries to Google module
    Example: <bot>, i'm feeling lucky <query>
    
    fixes #6

--- Bent.Bot/Module/Google.cs
+++ Bent.Bot/Module/Google.cs
@@ -13,7 +13,9 @@ namespace Bent.Bot.Module
     [Export(typeof(IModule))]
     public class Google : IModule
     {
-        private static Regex regex = new Regex(@"^\s*google\s+(.+?)\s*$", RegexOptions.IgnoreCase);
+        private static Regex regexGoogle = new Regex(@"^\s*google\s+(.+?)\s*$", RegexOptions.IgnoreCase);
+        private static Regex regexLucky = new Regex(@"^\s*i'?m\s+feeling\s+lucky\s+(.+?)\s*$", RegexOptions.IgnoreCase);
+
         private IBackend backend;
 
         public void OnStart(IConfiguration config, IBackend backend)
@@ -23,13 +25,32 @@ namespace Bent.Bot.Module
 
         public void OnMessage(IMessage message)
         {
-            if(message.IsRelevant)
+            TestGoogle(message);
+            TestLucky(message);
+        }
+
+        private void TestGoogle(IMessage message)
+        {
+            if (message.IsRelevant)
             {
-                var match = regex.Match(message.Body);
+                var match = regexGoogle.Match(message.Body);
 
                 if (match.Success)
                 {
                     this.backend.SendMessageAsync(message.ReplyTo, "http://lmgtfy.com/?q=" + HttpUtility.UrlEncode(match.Groups[1].Value));
+                }
+            }
+        }
+
+        private void TestLucky(IMessage message)
+        {
+            if (message.IsRelevant)
+            {
+                var match = regexLucky.Match(message.Body);
+
+                if (match.Success)
+                {
+                    this.backend.SendMessageAsync(message.ReplyTo, "http://lmgtfy.com/?l=1&q=" + HttpUtility.UrlEncode(match.Groups[1].Value));
                 }
             }
         }
